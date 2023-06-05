746b6b9c5ae001a02611ad974af49d26e618f477
Felipe Rímola <feliperimola9@gmail.com>
2023-06-05 17:57:06 -0300

    sub2

--- api/src/routes/controllers/getApiInfo.js
+++ api/src/routes/controllers/getApiInfo.js
@@ -1,21 +1,36 @@
 const axios = require ('axios');
 // const {API_KEY}= process.env;
-const API_KEY = '7e6610e2efc146e9ad2bd66e512f97fe';
+const API_KEY = '9f9b9dbd1a804ddabea15e6db712a786';
 const getApiInfo = async()=>{
     console.log('APIKEY ', API_KEY)
     try {
-        let array=[];    
-        for(let i=0; i<100; i++){
-            const recipe= await axios.get(`https://api.spoonacular.com/recipes/${i}/information?apiKey=${API_KEY}`);
-            array.push(recipe.data); 
-            console.log(recipe);
+        let arrayPromises=[];    
+        for(let i=1; i<10; i++){
+            const recipe= axios.get(`https://api.spoonacular.com/recipes/${i}/information?apiKey=${API_KEY}`).then(res => res.data, err => err.mensage);
+            arrayPromises.push(recipe); 
+            // console.log(recipe);
         }
-        return array;
-
+        const arrayResult = await Promise.all(arrayPromises).then(res => res);
+        console.log(arrayResult);
+        console.log('Cantidad de recetas: ', arrayResult.length);
+        const arrayFinal = arrayResult.map((recipe) => {
+            let newSteps = recipe?.analyzedInstructions[0]?.steps?.map((step)=>step.step);
+            console.log(recipe);
+            return recipe?{
+                id: recipe.id,
+                name: recipe.title,
+                summary: recipe.summary,
+                image: recipe.image,
+                diets: recipe.diets,
+                healthScore: recipe.healthScore,
+                steps: newSteps,
+            }:null;
+        })
+        let arrayFilter=arrayFinal.filter((recipe)=>recipe?true:false);
+        return arrayFilter;
     } catch (error) {
+        console.log('Error Funcion GetApiInfo');
         console.log(error);
     }
-//     const recipe=await axios.get(`https://api.spoonacular.com/recipes/9/information?apiKey=${API_KEY}`)
-//     return recipe.data;
 }
 module.exports=getApiInfo
--- api/src/routes/controllers/recipesRouter.js
+++ api/src/routes/controllers/recipesRouter.js
@@ -1,19 +1,32 @@
 const {Router}= require('express');
 const router=Router();
 const getApiInfo=require('./getApiInfo');
+const axios=require('axios');
+const API_KEY = '9f9b9dbd1a804ddabea15e6db712a786';
 
-router.get('/', async(req,res)=>{
+router.get('/:id',async(req,res)=>{
     try {
-        const allRecipes=await getApiInfo();
-        console.log('ALLRECIPES' ,allRecipes);
-        if(allRecipes.length!==0){
-            return res.status(200).send(allRecipes);
-        } else{
-            return res.status(404).send('Recipes not found');}
+        const {id}=req.body;
+        console.log(id, 'ID');
+        const recipeById=await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${API_KEY}`);
+        return res.status(200).send(recipeById.data);
     } catch (error) {
     console.log(error);
     res.status(404).send(error);
     }
 })
-
+router.get('/', async(req,res)=>{
+    try {
+        const allRecipes=await getApiInfo();
+        // console.log(getApiInfo);
+        // console.log('ALLRECIPES' ,allRecipes);
+        // if(allRecipes.length!==0){
+            return res.status(200).send(allRecipes);
+        // } else{
+        //     return res.status(404).send('Recipes not found');}
+    } catch (error) {
+    console.log(error);
+    res.status(404).send(error);
+    }
+})
 module.exports=router;
